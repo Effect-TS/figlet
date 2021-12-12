86ad7602a5d0bbf325e724c77faef8da0951ac10
Maxwell Brown <maxwellbrown1990@gmail.com>
2021-12-12 12:18:03 -0500

    chore: add simple examples

---
+++ packages/figlet/examples/client-api/simple.ts
@@ -0,0 +1,44 @@
+import * as T from "@effect-ts/core/Effect"
+import { pipe } from "@effect-ts/core/Function"
+import { runMain } from "@effect-ts/node/Runtime"
+
+import * as FigletClient from "../../src/FigletClient"
+import * as FontFileReader from "../../src/FontFileReader"
+import { RenderOptions } from "../../src/RenderOptions"
+import * as HorizontalLayout from "../../src/RenderOptions/HorizontalLayout"
+import * as Justification from "../../src/RenderOptions/Justification"
+import * as PrintDirection from "../../src/RenderOptions/PrintDirection"
+
+const renderOptions = pipe(
+  T.struct({
+    font: FigletClient.loadFontInternal("slant"),
+    maxWidth: T.succeed(120),
+    horizontalLayout: T.succeed(new HorizontalLayout.HorizontalSmushing()),
+    justification: T.succeed(new Justification.FontDefault()),
+    printDirection: T.succeed(new PrintDirection.LeftToRight())
+  }),
+  T.map((_) => new RenderOptions(_))
+)
+
+pipe(
+  renderOptions,
+  T.map((options) => FigletClient.renderToString("Hello, World!", options)),
+  T.chain((output) =>
+    T.succeedWith(() => {
+      console.log(output)
+    })
+  ),
+  T.provideSomeLayer(
+    FontFileReader.LiveFontFileReader[">>>"](FigletClient.LiveFigletClient)
+  ),
+  runMain
+)
+
+/**
+ *     __  __     ____           _       __           __    ____
+ *    / / / /__  / / /___       | |     / /___  _____/ /___/ / /
+ *   / /_/ / _ \/ / / __ \      | | /| / / __ \/ ___/ / __  / /
+ *  / __  /  __/ / / /_/ /      | |/ |/ / /_/ / /  / / /_/ /_/
+ * /_/ /_/\___/_/_/\____( )     |__/|__/\____/_/  /_/\__,_(_)
+ *                      |/
+ */
